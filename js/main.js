6b48c332e7ba865a4b7b24cf0eaf8e5e18d30fd2
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-06-09 22:18:15 -0300

    Aperfeicoando a usabilidade

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-const openModalBtn = document.getElementById("open-modal");
+const openModalBtns = document.querySelectorAll(".open-modal");
 const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementsByClassName("close")[0];
+const closeModalBtn = document.querySelector("#modal .close");
 
-openModalBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+openModalBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 });
 
 closeModalBtn.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
